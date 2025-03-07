83433be1b01e747a0267239d24ecf01090991b2e
sriram <159551306+sriram-65@users.noreply.github.com>
2025-03-07 15:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask on Vercel!"
+    return render_template("index.html")
 
-# Required for Vercel
-def handler(event, context):
-    return app(event, context)
+if __name__ == "__main__":
+    app.run()
